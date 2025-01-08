8e0fdbbe175ecb9b6eacb1761e5e481b07af4241
Julieta <jlancellottii23@fpcoruna.afundacion.org>
2025-01-08 11:04:23 +0100

    PokeReact

--- pokereact/src/components/PokeLista.js
+++ pokereact/src/components/PokeLista.js
@@ -1,17 +1,30 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const PokeLista = (props) =>{
     const [pokemons, setPokemons] = useState([])
-
+    const navigate = useNavigate();
     
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon")
     .then(response => {
+
+
         // console.log(response.data);
         setPokemons(response.data.results);
     });
     }, [])
+
+    const funcionNavegarASnivy = () =>{
+        navigate("/pokemon/495")
+    }
+
+    const funcionNavegar = (p) =>{
+        //navigate("/pokemon/" + p)
+        navigate(`/pokemon/${p}`)
+    }
 
     /*
     16 d ediciembre queda pendiente para 18 dic: 
@@ -24,9 +37,17 @@ const PokeLista = (props) =>{
     */
 
     return <div>
-        <h1>Lista: </h1>
-        { pokemons.map(p => <p>Bo nadal</p>)} 
-    </div>
+    <button onClick={funcionNavegaraSnivy}>Navegar a Snivy</button>
+          <link to= "/pokemon/25">Ir a PIKACHU</link>
+          <h1>Lista</h1>
+          {pokemons.map(p =>{
+            return <><p>Este pokemon es {p.name}</p>
+            <div onClick={()=>{navigate("/pokemon/" + p.name)}}>
+              Navegar</div>
+            <Link to={"pokemon/"+p.name}>Navegar</Link>
+            </>
+          })}
+      </div>
    
 }
 
